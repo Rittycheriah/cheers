b40ca8e4d24a084097a218cf230228a46bda6fac
rittycheriah <rittycheriah@gmail.com>
2015-07-18 09:36:57 -0500

    cheers working now

--- csharp/cheersRedux/cheersRedux/Program.cs
+++ csharp/cheersRedux/cheersRedux/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Linq;
 using System.Windows;
 using System.IO;
 
@@ -8,16 +9,13 @@ namespace cheersRedux
 	class cheersClass 
 	{
 		public static void Main(string[] args) 
-		{
-			string line; 
+		{ 
 			Console.WriteLine ("What is your name?");
 			var UserName = Console.ReadLine ();
 
-			char[] nameArray = new char[UserName.Length];
-
-		   	for (int i = 0; i < nameArray.length; i++) 
+			foreach (char i in UserName)
 		   	{
-				if (i == "a" || "e" || "o" || "i" || "u") 
+				if ("aeiou".Contains(i)) 
 				{
 					Console.WriteLine("Give me an...{0}", i);
 				} 
@@ -26,6 +24,10 @@ namespace cheersRedux
 					Console.WriteLine("Give me a...{0}", i);
 				}
 			}
+
+			Console.Write(UserName);
+			Console.Write (" is FABULOUS!");
+				
 		}
 	}
 }
